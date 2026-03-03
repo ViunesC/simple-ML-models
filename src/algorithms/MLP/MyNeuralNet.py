cd1a01892ec95ffc2fb07d7de620c0c27046b6a7
ViunesC <mrfusking233@gmail.com>
2026-03-03 18:10:07 -0500

    init empty template

---
+++ src/algorithms/MLP/MyNeuralNet.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class MyNeurons:
+    def __init__(self) -> None:
+        pass
+
+class MyLayer:
+    def __init__(self) -> None:
+        pass
+
+class MyNeuralNet:
+    def __init__(self) -> None:
+        pass
